0bc8c40170605fe0569a607cf77ccdd0b08f8ca4
huamei09 <1278806998@qq.com>
2020-02-05 10:19:59 +0800

    日期时间bug修改

--- src/view/risk/OperationAdd.vue
+++ src/view/risk/OperationAdd.vue
@@ -419,7 +419,7 @@
     },
     methods: {
       init: function () {
-          this.formData.registrateTime = this.getCurrentDate();
+          this.formData.registrateTime = this.getCurrentDate()
       },
       goBack: function () {
         this.$props.closeSelf && this.$props.closeSelf()
@@ -526,19 +526,14 @@
         fileList.splice(index, 1)
       },
       getCurrentDate: function () {
-          var date = new Date();
-          var seperator1 = "-";
-          var year = date.getFullYear();
-          var month = date.getMonth() + 1;
-          var strDate = date.getDate();
-          if (month >= 1 && month <= 9) {
-              month = "0" + month;
-          }
-          if (strDate >= 0 && strDate <= 9) {
-              strDate = "0" + strDate;
-          }
-          var currentdate = year + seperator1 + month + seperator1 + strDate;
-          return currentdate;
+        var date1=new Date();
+        var year=date1.getFullYear();
+        var month=date1.getMonth()+1;
+        var day=date1.getDate();
+        var hours=date1.getHours();
+        var minutes=date1.getMinutes();
+        var seconds=date1.getSeconds();
+        return year+seperator1+month+seperator1+day+seperator1+hours+":"+minutes+":"+seconds
       }
     }
   }
